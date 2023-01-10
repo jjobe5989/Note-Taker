9ae9db9a870cd5adb8b3a5dc5a07fbad8b9286b4
jjobe5989 <s3thjobe@gmail.com>
2023-01-09 19:47:58 -0600

    fixed bugs and added helpers and routes

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const app = express();
-var PORT = proces.env.PORT || 3001;
-app.use(express.static(__dirname + '/public'));
+// Dependencies
+const express = require("express");
 
+// Sets up the Express App
+const app = express();
+
+// Sets the port
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function () {
+  console.log(`App is listening on PORT: ${PORT}`);
+});
