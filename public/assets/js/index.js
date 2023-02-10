37a1d6190065204c46bd15ec28d32068f0b798f7
jjobe5989 <s3thjobe@gmail.com>
2023-02-09 20:27:14 -0600

    fixed errors in the index.js and server.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,8 @@
-const noteTitle = $(".note-title");
-const noteText = $(".note-textarea");
-const saveNoteBtn = $(".save-note");
-const newNoteBtn = $(".new-note");
-const noteList = $(".list-container .list-group");
+const $noteTitle = $(".note-title");
+const $noteText = $(".note-textarea");
+const $saveNoteBtn = $(".save-note");
+const $newNoteBtn = $(".new-note");
+const $noteList = $(".list-container .list-group");
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
@@ -37,23 +37,23 @@ const renderActiveNote = () => {
   $saveNoteBtn.hide();
 
   if (activeNote.id) {
-    noteTitle.attr("readonly", true);
-    noteText.attr("readonly", true);
-    noteTitle.val(activeNote.title);
-    noteText.val(activeNote.text);
+    $noteTitle.attr("readonly", true);
+    $noteText.attr("readonly", true);
+    $noteTitle.val(activeNote.title);
+    $noteText.val(activeNote.text);
   } else {
-    noteTitle.attr("readonly", false);
-    noteText.attr("readonly", false);
-    noteTitle.val("");
-    noteText.val("");
+    $noteTitle.attr("readonly", false);
+    $noteText.attr("readonly", false);
+    $noteTitle.val("");
+    $noteText.val("");
   }
 };
 
 // Get the note data from the inputs, save it to the db and update the view
 const handleNoteSave = function () {
   const newNote = {
-    title: noteTitle.val(),
-    text: noteText.val(),
+    title: $noteTitle.val(),
+    text: $noteText.val(),
   };
 
   saveNote(newNote).then(() => {
@@ -90,37 +90,33 @@ const handleNewNoteView = function () {
   activeNote = {};
   renderActiveNote();
 };
-
-// If a note's title or text are empty, hide the save button
-// Or else show it
+// Shows save button only when needed
 const handleRenderSaveBtn = function () {
-  if (noteTitle.val().trim() || noteText.val().trim()) {
-    saveNoteBtn.hide();
+  if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
+    $saveNoteBtn.hide();
   } else {
-    saveNoteBtn.show();
+    $saveNoteBtn.show();
   }
 };
 
-// Render's the list of note titles
+//list of note titles
 const renderNoteList = (notes) => {
-  noteList.empty();
+  $noteList.empty();
 
   const noteListItems = [];
 
-  // Returns jquery object for li with given text and delete button
-  // unless withDeleteButton argument is provided as false
   const create$li = (text, withDeleteButton = true) => {
-    const li = $("<li class='list-group-item'>");
-    const span = $("<span>").text(text);
-    li.append(span);
+    const $li = $("<li class='list-group-item'>");
+    const $span = $("<span>").text(text);
+    $li.append($span);
 
     if (withDeleteButton) {
-      const delBtn = $(
+      const $delBtn = $(
         "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
       );
-      li.append(delBtn);
+      $li.append($delBtn);
     }
-    return li;
+    return $li;
   };
 
   if (notes.length === 0) {
@@ -132,7 +128,7 @@ const renderNoteList = (notes) => {
     noteListItems.push($li);
   });
 
-  noteList.append(noteListItems);
+  $noteList.append(noteListItems);
 };
 
 // Gets notes from the db and renders them to the sidebar
@@ -140,12 +136,12 @@ const getAndRenderNotes = () => {
   return getNotes().then(renderNoteList);
 };
 
-saveNoteBtn.on("click", handleNoteSave);
-noteList.on("click", ".list-group-item", handleNoteView);
-newNoteBtn.on("click", handleNewNoteView);
-noteList.on("click", ".delete-note", handleNoteDelete);
-noteTitle.on("keyup", handleRenderSaveBtn);
-noteText.on("keyup", handleRenderSaveBtn);
+$saveNoteBtn.on("click", handleNoteSave);
+$noteList.on("click", ".list-group-item", handleNoteView);
+$newNoteBtn.on("click", handleNewNoteView);
+$noteList.on("click", ".delete-note", handleNoteDelete);
+$noteTitle.on("keyup", handleRenderSaveBtn);
+$noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
 getAndRenderNotes();
--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 // Dependencies
 const express = require("express");
-const path = require('path');
-const fs = require("fs");
-const util = require("util");
-// const api = require('./routes/noteRoutes')
 
 // Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+// Port
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use('/api', api);
 
 // Routes
 require("./routes/apiRoutes")(app);
